ff1b0a039ba870bda6ec5428528b23bb42d3fd12
Victor Whyte <victordavidwhyte@gmail.com>
2023-05-08 14:34:37 +0100

    feat: set up react query

--- src/components/ChatInput.tsx
+++ src/components/ChatInput.tsx
@@ -1,5 +1,8 @@
 'use client';
 import { cn } from '@/lib/utils';
+import { Message } from '@/lib/validators/message';
+import { useMutation } from '@tanstack/react-query';
+import { nanoid } from 'nanoid';
 import { FC, HTMLAttributes, useState } from 'react';
 import TextareaAutosize from 'react-textarea-autosize';
 
@@ -7,12 +10,41 @@ interface ChatInputProps extends HTMLAttributes<HTMLDivElement> {}
 
 const ChatInput: FC<ChatInputProps> = ({ className, ...props }) => {
   const [input, setInput] = useState<string>('');
+  const { mutate: sendMessage, isLoading } = useMutation({
+    mutationFn: async (message: Message) => {
+      const response = await fetch('/api/message', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ messages: 'hello' }),
+      });
+
+      return response.body;
+    },
+    onSuccess: () => {
+      console.log('Success');
+    },
+  });
 
   return (
     <div {...props} className={cn('border-t border-zinc-300', className)}>
       <div className="relative mt-4 flex-1 overflow-hidden rounded-lg border-none outline-none">
         <TextareaAutosize
           rows={2}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter' && !e.shiftKey) {
+              e.preventDefault();
+
+              const message: Message = {
+                id: nanoid(),
+                isUserMessage: true,
+                text: input,
+              };
+
+              sendMessage(message);
+            }
+          }}
           maxRows={4}
           value={input}
           onChange={(e) => setInput(e.target.value)}
